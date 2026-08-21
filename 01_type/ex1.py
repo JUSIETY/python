8eede86002e89387ca0f5bc67f7962bbc07237c8
권저웋ㄴ132245678901602 <dreamhun07@gmail.com>
2026-08-21 20:46:21 +0900

    first commit

---
+++ 01_type/ex1.py
@@ -0,0 +1,26 @@
+# 변수
+a = 2
+b = 3
+a, b = 2, 3
+print(a, b)
+temp = a
+a = b
+b = temp
+print(a, b)
+x = y = z = 0
+
+# 변수명 규칙
+# 숫자로 시작 불가
+# 예악어 사용 금지
+# 알파벳, 숫자 , 특수문자(_)만 가능
+# 대소문자 구분
+# 2name = "뽀로로"
+# !name = "krong"
+# class = "lufi"
+이름 = "에디"
+print(이름)
+
+student_name = "lufi" # snake_case
+studentName = "phobi" # camelCase
+
+MAX_SCORE = 100 # 상수는 대문짜로
