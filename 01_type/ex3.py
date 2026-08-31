a518098427a9a584104390c93ac18870329dc3cd
권저웋ㄴ132245678901602 <dreamhun07@gmail.com>
2026-08-31 11:25:52 +0900

    ex6.py

---
+++ 01_type/ex3.py
@@ -0,0 +1,22 @@
+# 불리언
+# ㅆ겯ㄷ, ㄹ;ㄴㄷ
+
+a = Trueprint(a, type(A))
+
+print(2 < 3)
+print(2 > 3)
+print(2 == 3)
+print(2 !=  3)
+
+print("apple" > "abple")
+
+print(bool(3))
+print(bool(0))"" [10] []
+print(bool("hello"))
+print("")
+print([10])
+print(10)
+
+a = None
+print(a, type(a))
+
